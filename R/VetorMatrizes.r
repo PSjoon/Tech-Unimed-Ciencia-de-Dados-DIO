226ab481187c44a7c744556d13ded9a8e99b2ebb
PSjoon <pedrosantos.joon@gmail.com>
2023-07-02 15:28:09 -0300

    R start

---
+++ R/VetorMatrizes.r
@@ -0,0 +1,9 @@
+(primeiro <- c(1,2,3,4,5))
+(segundo <- c(6,7,8,9,10))
+(terceiro <- c(primeiro,segundo))
+
+length(terceiro)
+#----------------------------------------------------------------
+
+A <- matrix(c(2,4,5,6,9,8,10), nrow = 2, ncol = 3, byrow = TRUE)
+A
---
+++ R/media.r
@@ -0,0 +1,7 @@
+x <- c(5,5,0,5,5)
+
+resul <- mean(x)
+resul
+
+mediana <- median(x)
+mediana
